abc47bee6984e98e7513f9e5fefb07866cd8ab6b
Pedro Pires <pedro.pires@xpand-it.com>
2016-02-28 22:01:11 +0000

    native android displaying all buttons in width

--- src/AndroidXamarinBenchmark/AndroidXamarinBenchmark/Activities/ActivityControlsVisualPerformance.cs
+++ src/AndroidXamarinBenchmark/AndroidXamarinBenchmark/Activities/ActivityControlsVisualPerformance.cs
@@ -17,7 +17,7 @@ namespace AndroidXamarinBenchmark.Activities
             LinearLayout linearLayoutWrapper = FindViewById<LinearLayout>(Resource.Id.linearLayout_wrapper);
 
             //TODO: test Out of memory
-            for (int j = 0; j < 500; j++)
+            for (int j = 0; j < 450; j++)
             {
                 LinearLayout linearLayout = new LinearLayout(this);
                 LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, LinearLayout.LayoutParams.WrapContent);
